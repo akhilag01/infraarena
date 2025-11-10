3ce736d288e6f56ad175704bae1e38e1325a35bd
akhilag01 <88706651+akhilag01@users.noreply.github.com>
2025-11-09 19:38:38 -0800

    Test absolute minimal handler

--- api/simple.py
+++ api/simple.py
@@ -1,13 +1,5 @@
-"""
-Absolute minimal test to see if basic handler works
-"""
-
 def handler(event, context):
-    """Minimal handler for testing"""
     return {
         "statusCode": 200,
-        "headers": {
-            "Content-Type": "application/json"
-        },
-        "body": '{"status": "ok", "message": "Minimal handler works!"}'
+        "body": "SIMPLE HANDLER WORKS"
     }
